d23329189f32ccaa4ed32d63ca5b3d02fbb4bae1
Lukas Fülling <lukas@k40s.net>
2015-01-24 19:08:34 +0100

    Added server deploy script

---
+++ deployOnServer.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+echo Building project...
+echo
+mvn -Dmaven.test.skip=true -DskipTests clean package
+echo
+service tomcat7 stop
+echo
+echo Deleting files:
+rm -vrf /var/lib/tomcat7/webapps/ROOT*
+echo
+echo Moving files:
+mv -v target/music-*.war /var/lib/tomcat7/webapps/Root.war
+echo
+service tomcat7 start
+echo
+echo Done.
+
