4693e1375278feb8a07a5862540b0cde1b3a0b5a
Lukas Fülling <lukas@k40s.net>
2015-02-05 19:52:13 +0100

    Prettified Albums Page; Fixed singles Page; DEPLOY

--- src/main/java/net/k40s/album/AlbumsPage.java
+++ src/main/java/net/k40s/album/AlbumsPage.java
@@ -19,52 +19,61 @@ import org.apache.wicket.request.resource.ContextRelativeResource;
 /**
  * @author Lukas F&uuml;lling (l.fuelling@micromata.de)
  */
-public class AlbumsPage extends WebPage implements Serializable{
-  public AlbumsPage() {
+public class AlbumsPage extends WebPage implements Serializable {
+    public AlbumsPage() {
 
-  }
+    }
 
-  @Override
-  protected void onInitialize() {
-    super.onInitialize();
-    add(new Link("homeLink") {
-      @Override
-      public void onClick() {
-        setResponsePage(HomePage.class);
-      }
-    });
-    add(new Link("singlesLink") {
-      @Override
-      public void onClick() {
-        setResponsePage(SinglesPage.class);
-      }
-    });
-    add(new Link("albumsLink") {
-      @Override
-      public void onClick() {
-        setResponsePage(AlbumsPage.class);
-      }
-    });
-    add(new ExternalLink("contactLink", "http://k40s.net"));
+    @Override
+    protected void onInitialize() {
+        super.onInitialize();
+        add(new Link("homeLink") {
+            @Override
+            public void onClick() {
+                setResponsePage(HomePage.class);
+            }
+        });
+        add(new Link("singlesLink") {
+            @Override
+            public void onClick() {
+                setResponsePage(SinglesPage.class);
+            }
+        });
+        add(new Link("albumsLink") {
+            @Override
+            public void onClick() {
+                setResponsePage(AlbumsPage.class);
+            }
+        });
+        add(new ExternalLink("contactLink", "http://k40s.net"));
 
-    add(new ListView<Album>("albums", Storage.getReversedAlbums()) { // getReversedSingles for correct placement on the website.
-      @Override
-      protected void populateItem(ListItem<Album> item) {
-        final Album album = item.getModelObject();
-        Link link = new Link("link") {
-          @Override
-          public void onClick() {
-            PageParameters params = new PageParameters();
-            params.add("id", album.getId());
-            setResponsePage(AlbumProductPage.class, params);
-          }
-        };
-        link.add(new Label("name", album.getName()));
-        link.add(new Image("image", new ContextRelativeResource("/album_images/" + album.getImage())));
-        item.add(link);
-      }
-    });
-    add(new Label("version", Storage.getVersion()));
-    
-  }
+        add(new ListView<Album>("albums", Storage.getReversedAlbums()) { // getReversedSingles for correct placement on the website.
+            @Override
+            protected void populateItem(ListItem<Album> item) {
+                final Album album = item.getModelObject();
+                Link link = new Link("link") {
+                    @Override
+                    public void onClick() {
+                        PageParameters params = new PageParameters();
+                        params.add("id", album.getId());
+                        setResponsePage(AlbumProductPage.class, params);
+                    }
+                };
+                Link link2 = new Link("link2") {
+                    @Override
+                    public void onClick() {
+                        PageParameters params = new PageParameters();
+                        params.add("id", album.getId());
+                        setResponsePage(AlbumProductPage.class, params);
+                    }
+                };
+                link2.add(new Label("name", album.getName()));
+                link.add(new Image("image", new ContextRelativeResource("/album_images/" + album.getImage())));
+                item.add(link);
+                item.add(link2);
+            }
+        });
+        add(new Label("version", Storage.getVersion()));
+
+    }
 }
